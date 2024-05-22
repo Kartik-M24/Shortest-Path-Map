f9a44ffc1b830a7d0350c2b5b5790c5498d42e7d
Kartik Malik <kmal768@aucklanduni.ac.nz>
2024-05-23 02:35:09 +1200

    Fixed exceptions method to use try and catch properly and fixed while loop accordingly

--- src/main/java/nz/ac/auckland/se281/InvalidCountryException.java
+++ src/main/java/nz/ac/auckland/se281/InvalidCountryException.java
@@ -1,8 +1,8 @@
 package nz.ac.auckland.se281;
 
-public class InvalidCountryException extends RuntimeException {
+public class InvalidCountryException extends Exception {
 
-  public InvalidCountryException(String country) {
-    super(MessageCli.INVALID_COUNTRY.getMessage(country));
+  public InvalidCountryException() {
+    super();
   }
 }
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -33,10 +33,10 @@ public class MapEngine {
   public boolean validCountryInput(String countryInput) {
     List<String> countries = Utils.readCountries();
     for (String country : countries) {
-      if (country.contains(countryInput)) {
-        String[] countryData = country.split(",");
+      String[] countryData = country.split(",");
+      if (countryData[0].equals(countryInput)) {
         countryName = countryData[0];
-        continent = countryData[1]; 
+        continent = countryData[1];
         taxFees = countryData[2];
         return true;
       }
@@ -44,18 +44,27 @@ public class MapEngine {
     return false;
   }
 
+  public boolean checkInputException(String countryInput) throws InvalidCountryException {
+    if (validCountryInput(countryInput) == false) {
+      throw new InvalidCountryException();
+    }
+    return true;
+  }
+
   /** this method is invoked when the user run the command info-country. */
-  public void showInfoCountry() throws InvalidCountryException {
+  public void showInfoCountry() {
     boolean isValid = false;
     while (isValid == false) {
       MessageCli.INSERT_COUNTRY.printMessage();
       countryInput = Utils.scanner.nextLine();
-      if (validCountryInput(countryInput) == false) {
-        throw new InvalidCountryException(countryInput);
+      countryInput = Utils.capitalizeFirstLetterOfEachWord(countryInput);
+      try {
+        isValid = checkInputException(countryInput);
+      } catch (InvalidCountryException e) {
+        MessageCli.INVALID_COUNTRY.printMessage(countryInput);
       }
-      isValid = true;
-      MessageCli.COUNTRY_INFO.printMessage(countryName, continent, taxFees);
     }
+    MessageCli.COUNTRY_INFO.printMessage(countryName, continent, taxFees);
   }
 
   /** this method is invoked when the user run the command route. */
